9f9d6cdd0a9c77161df53b15bccfdaa0edda443c
veroandi <veroandi.wcs@gmail.com>
2017-04-27 21:06:09 +0200

    show index

--- src/ChatBundle/Controller/DefaultController.php
+++ src/ChatBundle/Controller/DefaultController.php
@@ -2,16 +2,35 @@
 
 namespace ChatBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use ChatBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @param Request $request
+     * @Route("/", name="homepage")
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction()
+
+    public function indexAction(Request $request)
     {
-        return $this->render('ChatBundle:Default:index.html.twig');
+        $user = new User();
+
+        $form = $this->createFormBuilder($user)
+            ->add('name', TextType::class)
+            ->add('baseline', TextType::class)
+            ->add('submit', SubmitType::class, array('label' => 'Enter'))
+            ->getForm();
+
+        return $this->render('ChatBundle:Default:index.html.twig',
+            array(
+                'form' => $form->createView(),
+            ));
     }
 }
+
